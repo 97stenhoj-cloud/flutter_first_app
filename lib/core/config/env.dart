b3823893581f3106db34261e77c65889d37582ad
Markus Stenhøj <97stenhoj@gmail.com>
2025-11-22 14:21:37 +0100

    Fix: Add RevenueCat API keys to Env class
    - Added @EnviedField annotations for RevenueCat iOS and Android keys
    - Fixes undefined_getter errors in revenue_cat_service.dart
    - Keys are properly obfuscated using envied package

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -7,10 +7,16 @@ part 'env.g.dart';
 abstract class Env {
   @EnviedField(varName: 'SUPABASE_URL', obfuscate: true)
   static final String supabaseUrl = _Env.supabaseUrl;
-  
+
   @EnviedField(varName: 'SUPABASE_ANON_KEY', obfuscate: true)
   static final String supabaseAnonKey = _Env.supabaseAnonKey;
-  
+
   @EnviedField(varName: 'GOOGLE_WEB_CLIENT_ID', obfuscate: true)
   static final String googleWebClientId = _Env.googleWebClientId;
+
+  @EnviedField(varName: 'REVENUECAT_IOS_KEY', obfuscate: true)
+  static final String revenueCatIosKey = _Env.revenueCatIosKey;
+
+  @EnviedField(varName: 'REVENUECAT_ANDROID_KEY', obfuscate: true)
+  static final String revenueCatAndroidKey = _Env.revenueCatAndroidKey;
 }
